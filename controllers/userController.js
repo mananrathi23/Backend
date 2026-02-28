c730fd36b406b5957abee3707b1525c01fd91127
mananrathi23 <144107804+mananrathi23@users.noreply.github.com>
2026-02-28 16:34:37 +0530

    backend on git

---
+++ controllers/userController.js
@@ -0,0 +1,302 @@
+import ErrorHandler from "../middlewares/error.js";
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
+import { User } from "../models/userModel.js";
+import { sendEmail } from "../utils/sendEmail.js";
+import { generateEmailTemplate } from "../utils/emailTemplate.js";
+import { sendToken } from "../utils/sendToken.js";
+import crypto from "crypto";
+
+
+export const register = catchAsyncError(async (req, res, next) => {
+  try {
+    const { name, email, phone, password, verificationMethod } = req.body;
+    if (!name || !email || !phone || !password || !verificationMethod) {
+      return next(new ErrorHandler("All fields are required.", 400));
+    }
+    function validatePhoneNumber(phone) {
+      const phoneRegex = /^\+91[6-9]\d{9}$/;
+      return phoneRegex.test(phone);
+    }
+
+    if (!validatePhoneNumber(phone)) {
+      return next(new ErrorHandler("Invalid phone number.", 400));
+    }
+
+    const existingUser = await User.findOne({
+      $or: [
+        {
+          email,
+          accountVerified: true,
+        },
+        {
+          phone,
+          accountVerified: true,
+        },
+      ],
+    });
+
+    if (existingUser) {
+      return next(new ErrorHandler("Phone or Email is already used.", 400));
+    }
+
+    const registerationAttemptsByUser = await User.find({
+      $or: [
+        { phone, accountVerified: false },
+        { email, accountVerified: false },
+      ],
+    });
+
+    if (registerationAttemptsByUser.length > 3) {
+      return next(
+        new ErrorHandler(
+          "You have exceeded the maximum number of attempts (3). Please try again after an hour.",
+          400
+        )
+      );
+    }
+
+    const userData = {
+      name,
+      email,
+      phone,
+      password,
+    };
+
+    const user = await User.create(userData);
+    const verificationCode = await user.generateVerificationCode();
+    await user.save();
+    sendVerificationCode(
+      verificationMethod,
+      verificationCode,
+      name,
+      email,
+      phone,
+      res
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+
+async function sendVerificationCode(
+  verificationMethod,
+  verificationCode,
+  name,
+  email,
+  phone,
+  res
+) {
+  try {
+    if (verificationMethod === "email") {
+      const message = generateEmailTemplate(verificationCode);
+      sendEmail({ email, subject: "Your Verification Code", message });
+      res.status(200).json({
+        success: true,
+        message: `Verification email successfully sent to ${name}`,
+      });
+    } else if (verificationMethod === "phone") {
+      const verificationCodeWithSpace = verificationCode
+        .toString()
+        .split("")
+        .join(" ");
+      await client.calls.create({
+        twiml: `<Response><Say>Your verification code is ${verificationCodeWithSpace}. Your verification code is ${verificationCodeWithSpace}.</Say></Response>`,
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: phone,
+      });
+      res.status(200).json({
+        success: true,
+        message: `OTP sent.`,
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        message: "Invalid verification method.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Verification code failed to send.",
+    });
+  }
+}
+
+export const verifyOTP = catchAsyncError(async (req, res, next) => {
+  const { email, otp, phone } = req.body;
+  function validatePhoneNumber(phone) {
+    const phoneRegex = /^\+91[6-9]\d{9}$/;
+    return phoneRegex.test(phone);
+  }
+
+  if (!validatePhoneNumber(phone)) {
+    return next(new ErrorHandler("Invalid phone number.", 400));
+  }
+
+  try {
+    const userAllEntries = await User.find({
+      $or: [
+        { email, accountVerified: false },
+        { phone, accountVerified: false },
+      ],
+    }).sort({ createdAt: -1 });
+
+    if (!userAllEntries) {
+      return next(new ErrorHandler("User Not Found", 404));
+    }
+
+    let user;
+
+    if (userAllEntries.length > 1) {
+      user = userAllEntries[0];
+
+      await User.deleteMany({
+        _id: { $ne: user._id },
+        $or: [
+          { phone, accountVerified: false },
+          { email, accountVerified: false },
+        ]
+      })
+    }  // deleting the old entering and keeping the latest entry
+    else {
+      user = userAllEntries[0];
+    }
+
+    if (user.verificationCode !== Number(otp)) {
+      return next(new ErrorHandler("Invalid OTP", 400));
+    }
+    const currentTime = Date.now();  // Date format : 21324351
+    const verificationCodeExpire = new Date(User.verificationCodeExpire).getTime(); // time format : 23:23:11t12t1:123
+
+    console.log(currentTime);
+    console.log(verificationCodeExpire);
+
+    if (currentTime > verificationCodeExpire) {
+      return next(new ErrorHandler("OTP Expired.", 400));
+    }
+
+    user.accountVerified = true;
+    user.verificationCode = null;
+    user.verificationCodeExpire = null;
+    await user.save({ validateModifiedOnly: true });
+
+    sendToken(user, 200, "Account Verfied", res);
+
+  } catch (error) {
+    return next(new ErrorHandler("Internal Server Error", 500));
+  }
+});
+
+
+export const login = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ErrorHandler("Email or Password is required", 400));
+  }
+
+  const user = await User.findOne({ email, accountVerified: true }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or Password.", 400));
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or Password.", 400));
+  }
+
+  sendToken(user, 200, "User Logged In sucessfully", res);
+})
+
+
+export const logout = catchAsyncError(async (req, res, next) => {
+  res
+    .status(200)
+    .clearCookie("token", {
+      httpOnly: true,
+      path: "/",
+    })
+    .json({
+      success: true,
+      message: "Logged out successfully.",
+    });
+});
+
+export const getUser = catchAsyncError(async (req, res, next) => {
+  const user = req.user;
+  res.status(200).json({
+    success: true,
+    user,
+  })
+})
+
+export const forgotPassword = catchAsyncError(async (req, res, next) => {
+  const user = await User.findOne({
+    email: req.body.email,
+    accountVerified: true,
+  });
+  if (!user) {
+    return next(new ErrorHandler("User not found.", 404));
+  }
+  const resetToken = user.generateResetPasswordToken();
+  await user.save({ validateBeforeSave: false });
+  const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
+
+  const message = `Your Reset Password Token is:- \n\n ${resetPasswordUrl} \n\n If you have not requested this email then please ignore it.`;
+
+  try {
+    sendEmail({
+      email: user.email,
+      subject: "Alumni Portal Reset Password",
+      message,
+    });
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} successfully.`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(
+      new ErrorHandler(
+        error.message ? error.message : "Cannot send reset password token.",
+        500
+      )
+    );
+  }
+});
+
+
+export const resetPassword = catchAsyncError(async (req, res, next) => {
+  const { token } = req.params;
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+  if (!user) {
+    return next(
+      new ErrorHandler(
+        "Reset password token is invalid or has been expired.",
+        400
+      )
+    );
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(
+      new ErrorHandler("Password & confirm password do not match.", 400)
+    );
+  }
+
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+  await user.save();
+
+  sendToken(user, 200, "Reset Password Successfully.", res);
+});
